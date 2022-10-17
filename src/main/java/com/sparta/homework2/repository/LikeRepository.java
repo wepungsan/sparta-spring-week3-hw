9f4b6801f47c41378997c9e0a044224187316325
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-10-17 15:04:05 +0900

    like

---
+++ src/main/java/com/sparta/homework2/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.homework2.repository;
+
+import com.sparta.homework2.model.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+}
